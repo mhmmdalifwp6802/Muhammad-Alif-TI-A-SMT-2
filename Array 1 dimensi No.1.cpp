8a2eb2205c54b2982f2668986da7809a6a96d91d
mhmmdalifwp6802 <97727851+mhmmdalifwp6802@users.noreply.github.com>
2022-02-25 15:15:11 +0700

    Add files via upload

---
+++ Array 1 dimensi No.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nilai[5]={16,18,20,17,60};
+	int i;
+	for (i=0;i<5;i++){
+		cout <<"Nilai Ke-"<<i+1<<" = "<<nilai[i]<<endl;
+	}
+	return 0;
+}
